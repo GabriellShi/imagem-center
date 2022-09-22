ed1f5ca7a325952f20df9a450b652f5fdf9134fd
Gabriel Oliveira <gabrieloliveirasantos196@gmail.com>
2022-09-21 23:16:33 -0300

    Atualização Paginas, atualização Header Menu e Linkagens

--- src/controllers/orcamentosController.js
+++ src/controllers/orcamentosController.js
@@ -1,8 +1,48 @@
+
+
 const orcamentosController = {
     // Pode retornar uma página ou não
     index: (req, res) => {
         return res.render("orcamento", {title: "Orçamento", user: req.cookies.user,})
     },
+
+    viewComoChegar: (req, res) => {
+        res.render('como-chegar', { title: "Como Chegar", user: req.cookies.user, });
+    },
+
+    viewBannerFaixa: (req, res) => {
+        res.render('banner-faixa', { title: "Orçamento Banner e Faixa", user: req.cookies.user, });
+    },
+
+    viewCopiaProjetos: (req, res) => {
+        res.render('copia-projetos', { title: "Orçamento de Cópia de Projetos", user: req.cookies.user, });
+    },
+
+    viewCopia: (req, res) => {
+        res.render('copia', { title: "Orçamento de Cópias", user: req.cookies.user, });
+    },
+
+    viewCriacaoDeArte: (req, res) => {
+        res.render('criacao-de-arte', { title: "Orçamento Criação de Arte", user: req.cookies.user, });
+    },
+
+    viewDigitalizacao: (req, res) => {
+        res.render('digitalizacao', { title: "Orçamento Digitalizacao", user: req.cookies.user, });
+    },
+
+    viewEncadernacao: (req, res) => {
+        res.render('encadernacao', { title: "Orçamento Encadernacao", user: req.cookies.user, });
+    },
+
+    viewImpressaoProjetos: (req, res) => {
+        res.render('impressao-projetos', { title: "Orçamento de Impressão de Projetos", user: req.cookies.user, });
+    },
+
+    viewImpressao: (req, res) => {
+        res.render('impressao', { title: "Orçamento de Impressão", user: req.cookies.user, });
+    },
+    
+
 };
 
 module.exports = orcamentosController;
--- src/routes/orcamentosRoute.js
+++ src/routes/orcamentosRoute.js
@@ -4,7 +4,25 @@ const router = express.Router();
 // Controllers
 const orcamentosControlller = require('../controllers/orcamentosController');
 
+
 router.get("/orcamento", orcamentosControlller.index);
 
+router.get("/como-chegar", orcamentosControlller.viewComoChegar);
+
+router.get("/banner-faixa", orcamentosControlller.viewBannerFaixa);
+
+router.get("/copia-projetos", orcamentosControlller.viewCopiaProjetos);
+
+router.get("/copia", orcamentosControlller.viewCopia);
+
+router.get("/criacao-de-arte", orcamentosControlller.viewCriacaoDeArte);
+
+router.get("/digitalizacao", orcamentosControlller.viewDigitalizacao);
+
+router.get("/encadernacao", orcamentosControlller.viewEncadernacao);
+
+router.get("/impressao-projetos", orcamentosControlller.viewImpressaoProjetos);
+
+router.get("/impressao", orcamentosControlller.viewImpressao);
 
 module.exports = router;
